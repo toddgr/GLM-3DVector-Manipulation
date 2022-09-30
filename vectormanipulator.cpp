91e24f7ef135aaa7fab434cb9cc27ecbc1cbf447
gracey-todd <toddgr@oregonstate.edu>
2022-09-29 20:48:01 -0700

    Set up functions

--- vectormanipulator.cpp
+++ vectormanipulator.cpp
@@ -1,3 +1,17 @@
+/* 	Grace Todd
+	CS 491: Simulation and Game Programming
+	Fall 2022
+
+	Project 1: Using GLM to manipulate 3D Vectors
+	To Compile: g++ -o proj1 vectormanipulator.cpp
+				./proj1
+*/
+
+/********************************
+			HEADER FILES
+	(Comment out for submission)
+*********************************/
+
 #include <stdio.h>
 #include <string>
 #define _USE_MATH_DEFINES
@@ -10,38 +24,43 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/matrix_inverse.hpp"
 
+
 glm::vec3
 WhatPartOfALivesInBDir( glm::vec3 a, glm::vec3 b )
 {
-	???
+	glm::vec3 c;
+	return c;
 }
 
 
 glm::vec3
 WhatPartOfALivesPerpToB( glm::vec3 a, glm::vec3 b )
 {
-	???
+	glm::vec3 c;
+	return c;
 }
 
 
 glm::vec3
 UnitSurfaceNormal( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
-	 ???
+	glm::vec3 t;
+	return t;
 }
 
 
 float
 Area( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
-	 ???
+	float area;
+	return area;
 }
 
 
 bool
 IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
-	???
+	return true;
 }
 
 
@@ -49,13 +68,25 @@ IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 float
 DistanceFromPointToPlane( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
-	???
+	float distance;
+	return distance;
 }
 
 
 void
 WhoAmI( std::string &yourName, std::string &yourEmailAddress )
 {
-	yourName = "Joe Graphics";
-	yourEmailAddress = "jgraphics@oregonstate.edu" ;
+	yourName = "Grace Todd";
+	yourEmailAddress = "toddgr@oregonstate.edu" ;
+}
+
+
+/*******************************
+			MAIN PROGRAM
+	(Comment out for submission)
+********************************/
+int
+main() {
+	printf("Main works :P\n");
+	return 0;
 }
