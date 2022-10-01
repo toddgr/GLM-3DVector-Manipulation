e6617d0986ba32b0cf153ea2df5ee0c9d6a178b3
gracey-todd <toddgr@oregonstate.edu>
2022-09-30 19:02:31 -0700

    Comment out #include files

--- proj01.cpp
+++ proj01.cpp
@@ -14,17 +14,17 @@
 	(Comment out for submission)
 *********************************/
 
-#include <stdio.h>
-#include <string>
-#define _USE_MATH_DEFINES
-#include <cmath>
+// #include <stdio.h>
+// #include <string>
+// #define _USE_MATH_DEFINES
+// #include <cmath>
 
-#define GLM_FORCE_RADIANS
-#include "glm/vec2.hpp"
-#include "glm/vec3.hpp"
-#include "glm/mat4x4.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/matrix_inverse.hpp"
+// #define GLM_FORCE_RADIANS
+// #include "glm/vec2.hpp"
+// #include "glm/vec3.hpp"
+// #include "glm/mat4x4.hpp"
+// #include "glm/gtc/matrix_transform.hpp"
+// #include "glm/gtc/matrix_inverse.hpp"
 
 
 glm::vec3
